205648d1ae39b086abdd0c69e5e55c09fd69b293
DylanGuedes <djmgguedes@gmail.com>
2015-04-20 09:51:20 -0300

    new admin system

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,9 @@ Rails.application.routes.draw do
   post '/signin' => 'sessions#create'
   delete '/signout' => 'sessions#destroy'
 
+  #admin panel controller
+  get '/admin_panel' => 'admin_panel#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
